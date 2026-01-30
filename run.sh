c417cd3b08c965fc2ba6578c9c0fa87dddf79a1e
P. Radmard <pr450@mirzakhani.eng.cam.ac.uk>
2026-01-30 16:05:12 +0000

    added data

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=1
 
-# python -m generate_transcripts --run-name test --model-name Qwen/Qwen3-4B-Instruct-2507 --batch-size 4
-python -m capture_activations --model-name Qwen/Qwen3-4B-Instruct-2507 --batch-size 4 --transcript-dir outputs/transcripts/test_20260130_105234/Qwen_Qwen3-4B-Instruct-2507/main
+# python -m generate_transcripts --run-name test --model-name allenai/Olmo-3-7B-Instruct --batch-size 4
+python -m capture_activations --model-name allenai/Olmo-3-7B-Instruct --batch-size 4 --transcript-dir outputs/transcripts/test_20260130_110725/allenai_Olmo-3-7B-Instruct/main
 
